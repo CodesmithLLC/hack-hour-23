cacf4a3c0330a7f49fe56657b079a034dc2ec8b6
SelcukAtes <sselcukates@gmail.com>
2018-07-25 09:30:59 -0700

    "It is done"

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,20 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+    const arr = [];
+    for(let i = 1; i < 17; i++) {
+        if(i % 3 === 0 && i % 5 === 0) {
+            arr.push("fizzbuzz");
+        } else if (i % 3 === 0 ) {
+            arr.push("fizz")
+        } else if(i % 5 === 0) {
+            arr.push("buzz")
+        } else {
+            arr.push(i)
+        }
+    }
+    console.log(arr);
 }
-
+//fizzbuzz(16)
 module.exports = fizzbuzz;
+
