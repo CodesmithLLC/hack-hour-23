7f2ee476d1436dd13393fcfd75a7947f240851b0
Jonathan <jonathan.che@outlook.com>
2018-07-25 09:33:15 -0700

    hack-hour fizzbuzz completed

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -17,7 +17,22 @@
 //                     16 ]
 
 function fizzbuzz(num) {
-
+  // create an empty array to hold values
+  const result = [];
+  // iterate from 1 until num and populate the array
+  for (let i = 1; i <= num; i++) {
+    // special cases:
+    if (i % 3 === 0 && i % 5 === 0) { // put fizzbuzz in place of numbers divisible by 3 & 5
+      result.push('fizzbuzz');
+    } else if (i % 5 === 0) { // put buzz in place of numbers divisible by 5
+      result.push('buzz');
+    } else if (i % 3 === 0) { // put fizz in place of numbers divisible by 3
+      result.push('fizz');
+    } else {
+      result.push(i);
+    }
+  }
+  return result;
 }
 
 module.exports = fizzbuzz;
