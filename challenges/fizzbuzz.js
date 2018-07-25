af4f6f326f21a5d053ce414c506c367c1b0bd4b9
seano <sth.accounts@gmail.com>
2018-07-25 09:35:02 -0700

    done, and refactored

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -16,8 +16,47 @@
 //                     'fizzbuzz',
 //                     16 ]
 
-function fizzbuzz(num) {
 
+//success conditions: %3=0's and %5=0's are replaced by fizz and buzz, and &&'s by fizzbuzz
+//i will iterate to i=<num, starting at i=1, testing each one by a condition for %3, %5, and both. I think i should do both first, then each individually.
+//then I will push i to the array, unless it is divisible by either one.
+
+function fizzbuzz(num) {
+  return createFizzBuzzArrayAndReplace(num);
 }
+
+function createFizzBuzzArrayAndReplace(lastNum){
+  const arr = [];
+  for (let i=1; i <= lastNum; i++){
+    arr.push(getAppropriateElement(i));
+  }
+  return arr;
+}
+
+function getAppropriateElement(num){
+  if (divisibleByFiveAndThree(num)){
+    return 'fizzbuzz'
+  }else if (divisibleByThree(num)) {
+    return 'fizz'
+  }else if (divisibleByFive(num)){
+    return 'buzz'
+  }
+  return num;
+}
+
+function divisibleByFiveAndThree(num){
+  return num % 3 === 0 && num % 5 === 0
+}
+
+function divisibleByThree(num){
+  return num % 3 === 0;
+}
+
+function divisibleByFive(num){
+  return num % 5 === 0;
+}
+
+
+console.log(fizzbuzz(16))
 
 module.exports = fizzbuzz;
