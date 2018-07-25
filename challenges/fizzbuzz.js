0e927f5a3378ef9e73fb655c1fc26d4741ad3892
Colin Roemer <colinr@Colins-MacBook-Pro.local>
2018-07-25 10:17:16 -0700

    refactored fizzbuzz

--- challenges/fizzbuzz.js
+++ challenges/fizzbuzz.js
@@ -25,23 +25,10 @@
 // Output: an array - the array will cotain indexes for each number from 1 to the number paramater - except for the exceptions above
 
 function fizzbuzz(num) {
-  //Declare output array
   const output = [];
-
-  //run a for loop from 1 to num
   for (var i = 1; i < num + 1; i++) {
-    if (i % 5 === 0 && i % 3 === 0) {
-      output.push('fizzbuzz');
-    } else if (i % 3 === 0) {
-      output.push('fizz');
-    } else if (i % 5 === 0) {
-      output.push('buzz');
-    } else {
-      output.push(i);
-    }
+    output.push((i%3)?(i%5)?i:'Buzz':(i%5)?'Fizz':'FizzBuzz');
   }
-  
-  //return output array
   return output
 }
 
