6cc2baf77cd8d85ca726c75afff1e12db09159bc
gerretkubota <gtkubota@cpp.edu>
2018-07-27 10:04:14 -0700

    Not workig properly

--- challenges/draw-stairs.js
+++ challenges/draw-stairs.js
@@ -14,8 +14,43 @@
 */
 
 function drawStairs(n) {
+    // base case
+    // staircase must climb up from left to right
+    // last line should only have *, no leading/trailing spaces
+    // initially 5 spaces with the last being the *
+    // 4 spaces with **
+    // 3 spaces with ***
 
+    // if n === 3
+    // 2 spaces   * -> n-1 2  n - (n-1) prints
+    // 1 space   ** -> n-1 1
+    // 0 spaces *** -> n-1 0
+    let str = '';
+
+    if(n === 0){
+        for(let i = 0; i < n; i++){
+            str += '*';
+        }
+        console.log(str);  
+    }else{
+        // for spaces?
+        for(let i = 0; i < (n - 1); i++){
+            str += ' ';
+        }
+        // for *
+        for(let i = 0; i < (n - (n - 1)); i++){
+            str += '*';
+        }
+        console.log(str);
+        drawStairs(n - 1);
+    }
+    // let count = 0;
+    // while(count > n){
+    //     newStr += 
+    // }
 }
+
+drawStairs(3);
 
 
 module.exports = drawStairs;
