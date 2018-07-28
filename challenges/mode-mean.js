4f8f0b4abcbde3a032fcd8c1bd075c2ab9746d82
gerretkubota <gtkubota@cpp.edu>
2018-07-28 10:11:27 -0700

    Finished

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,34 @@
 
 
 function modemean(array) {
+  array.sort();
+  // mean
+  let mean = Math.floor(array.reduce((acc, currValue) => {
+    return acc += currValue;
+  }, 0) / 2);
+  
+  // mode repeated numbers
+  let mode = array.reduce((acc, currValue) => {
+    if(acc[currValue]){
+      ++newObj[currValue];
+    }else{
+      acc[currValue] = 1;
+    }
+    return acc;
+  }, {});
 
+  let modeMax = 0;
+  for(let key in value){
+    if(value[key] > modeMax)
+      modeMax = value[key];
+  }
+
+  if(modeMax === mean)
+    return true;
+  else
+    return false;
 }
 
 module.exports = modemean;
+
+
