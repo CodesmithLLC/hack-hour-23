f620ed3c4520ed22105e1c012e0419e5fd7dafde
JChan <jchan@JCs-MacBook-Pro.local>
2018-07-28 09:46:34 -0700

    completed

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,27 @@
  *
  */
 
+const num = [1, 2, 3, 4, 5, 6, 5];
+
+modemean(num);
 
 function modemean(array) {
+  let total = 0;
+  array.forEach(e => total += e);
 
+  const mean = Math.floor(total / array.length);
+  const obj = {};
+  array.forEach((e) => {
+    if (obj[e] === undefined) {
+      obj[e] = 1;
+    } else {
+      obj[e]++;
+    }
+  });
+  console.log(Object.values(obj));
+  const mode = Math.max(...Object.values(obj));
+  if (mean === mode) return true;
+  return false;
 }
 
 module.exports = modemean;
