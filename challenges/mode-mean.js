4b4d37403c051469b63b510403191409c7bb27a8
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-07-28 10:13:19 -0700

    initial commit

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -11,7 +11,26 @@
 
 
 function modemean(array) {
+   //exception cases
+   if (array.length === 0) return false;
+   if (array.length === 1) return true;
 
+   const mean = Math.floor(array.reduce((acc, next) => acc + next) / array.length);
+   //find count for every num in array
+   const counter = {};
+   array.forEach(num => {
+      if (counter[num] === undefined) counter[num] = 1;
+      else counter[num] += 1;
+   })
+   //cycle through the counts to find the mode
+   let mode;
+   for (let key in counter) {
+      if (!mode) mode = key;
+      else if (counter[key] > counter[mode]) mode = key;
+      else if (counter[key] === counter[mode] && key > mode) mode = key;
+   }
+   //mode will be a string, mean will be a num
+   return mode == mean;
 }
 
 module.exports = modemean;
