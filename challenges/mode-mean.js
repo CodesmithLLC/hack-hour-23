8ae5e680327ee9585c75cd79fc6105d8d5d7b3a0
andwong91 <andwong91@gmail.com>
2018-07-28 10:04:00 -0700

    Completed modemean

--- challenges/mode-mean.js
+++ challenges/mode-mean.js
@@ -9,9 +9,24 @@
  *
  */
 
-
 function modemean(array) {
-
+  const mean = Math.floor(array.reduce((acc, cur) => { return acc + cur }) / array.length);
+  const modeObj = {};
+  const modeArr = [];
+  let quantity = 0;
+  let mode = 0;
+  array.forEach((num) => {
+    if (!modeObj[num]) modeObj[num] = 1;
+    else modeObj[num] += 1;
+  });
+  const values = Object.values(modeObj);
+  const keys = Object.keys(modeObj);
+  quantity = Math.max(...values);
+  keys.forEach((key) => {
+    if (modeObj[key] === quantity) modeArr.push(parseInt(key));
+  });
+  mode = Math.max(...modeArr);
+  return mean === mode;
 }
 
 module.exports = modemean;
