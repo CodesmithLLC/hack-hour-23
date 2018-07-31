32a205997031801a79741aa14d2fa44ef7840121
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-07-31 09:28:36 -0700

    initial commit of kth-to-last-node

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,33 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+   let count = 0;
+   let value;
 
+   function search(head) {
+      //recurse until we reach the tail of the list
+      if (head.next !== null) search(head.next);
+
+      if (value === undefined) {
+         count += 1;
+         if (count === k) value = head.value;
+      }
+   }
+   
+   search(head);
+   return value;
 }
 
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e
+
+console.log(kthToLastNode(2, a));
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
