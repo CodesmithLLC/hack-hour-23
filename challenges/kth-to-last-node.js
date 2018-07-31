769fa6cbad1937396925e2f29f73a0125533ed3b
gerretkubota <gtkubota@cpp.edu>
2018-07-31 09:37:08 -0700

    I hope it works

--- challenges/kth-to-last-node.js
+++ challenges/kth-to-last-node.js
@@ -22,7 +22,35 @@ function Node(val) {
 }
 
 function kthToLastNode(k, head) {
+  let temp = head;
+  let temp2 = head;
+  let size = 0;
+  
+  while(temp != null){
+    temp = temp.next;
+    size++;
+  }
 
+  size -= k;
+  while(size > 0){
+    temp2 = temp2.next;
+    size--
+  }
+
+  return temp2.value;
 }
+
+
+const a = new Node('A');
+const b = new Node('B');
+const c = new Node('C');
+const d = new Node('D');
+const e = new Node('E');
+
+a.next = b;
+b.next = c;
+c.next = d;
+d.next = e;
+console.log(kthToLastNode(2, a)); //-> returns 'D' (the value on the second to last node)
 
 module.exports = {Node: Node, kthToLastNode: kthToLastNode};
