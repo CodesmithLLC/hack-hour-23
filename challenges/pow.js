fab34b4d72cb3df793991d23bb6426618c89aca9
Patrick Slagle <slaglebagel17@gmail.com>
2018-08-01 09:29:07 -0700

    completed pow hack hour, week 2, day 3

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,7 +3,12 @@
  */
 
 function pow(base, power) {
-
+    if (power === 0) return 1
+    if (power === 1) return base
+    let answer = base * pow(base, --power)
+    return answer; 
 }
+
+console.log(pow(3,4))
 
 module.exports = pow;
