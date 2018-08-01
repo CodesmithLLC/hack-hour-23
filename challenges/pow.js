a8cb9e7b2bc92ec0110420ab5f0d176c67c56b56
Andy Ahn <andyahn91@gmail.com>
2018-08-01 09:24:41 -0700

    pow_Updated (if base is 0 return 1)

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,8 +3,12 @@
  */
 
 function pow(base, power) {
-  if (power === 1) return base
-  return base*pow(base, power-1)
+  if(power === 0) {
+    return 1
+  } else if (power === 1) {
+    return base;
+  }
+  return base*pow(base, power-1);
 }
 
 module.exports = pow;
