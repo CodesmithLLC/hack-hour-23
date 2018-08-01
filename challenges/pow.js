5893e2c57e1d089c7939a3c15225f58a456c1ce8
andwong91 <andwong91@gmail.com>
2018-08-01 09:26:46 -0700

    Fixed pow.js to handle negative value powers

--- challenges/pow.js
+++ challenges/pow.js
@@ -3,8 +3,9 @@
  */
 
 function pow(base, power) {
-  if (power === 1) return base;
-  return base * pow(base, power - 1);
+  if (power === 0) return 1;
+  if (power > 0) return base * pow(base, power - 1);
+  else if (power < 0) return 1 / base * pow(base, power + 1);
 }
 
 module.exports = pow;
