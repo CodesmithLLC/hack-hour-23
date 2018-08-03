c898cce657b15004a3fc7941c28af954854c06bf
James Sieu <jamessieu@gmail.com>
2018-08-03 10:07:31 -0700

    try this again

--- challenges/reverse-in-place.js
+++ challenges/reverse-in-place.js
@@ -15,12 +15,12 @@
 
 function reverseInPlace(array) {
     let length = array.length;
-    console.log(length);
+    //console.log(length);
     const newArr = [];
     for(let x= length-1; x >=0; x--){
         newArr.push(array[x]);
     }
     return newArr.join('');
 }
-console.log(reverseInPlace('hellos'));
+//console.log(reverseInPlace('hellos'));
 module.exports = reverseInPlace;
