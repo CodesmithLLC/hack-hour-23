e928bce5615d621dfcf6bfe4957948fe27d23e4e
Jonathan <jonathan.che@outlook.com>
2018-08-04 09:59:00 -0700

    string-rotation challenge done 8/4/18

--- challenges/string-rotation.js
+++ challenges/string-rotation.js
@@ -15,7 +15,23 @@ function isSubstring(s1, s2) {
   return s1.indexOf(s2) >= 0;
 }
 
-function stringRotation(s1, s2) {
+function stringRotation(s1, s2, counter = 1) {
+  // make sure that the second string has the same length as the first string 
+  if (s1.length !== s2.length) return false;
+
+  // return false if counter === length of string, means all string rotation possibilities have been checked
+  if (s2.length === counter) return false 
+
+  // check to see if call to isSubstring returns true/false with current s2 input
+  if (!isSubstring(s1, s2)) {
+    // rotate the string, pass it in your recursive call
+    let firstElementOfString2 = s2[0];
+    let rotatedString2 = s2.slice(1) + firstElementOfString2;
+    return stringRotation(s1, rotatedString2, counter+1)
+  } else {
+    // return true, string 2 is a rotation of string 1
+    return true
+  }
 
 }
 
