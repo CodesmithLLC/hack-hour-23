42f6c0bef95c1b1c3bf1a064e8bb428e08c70b17
G0ldenSp00n <hayfithyan@gmail.com>
2018-08-07 09:55:59 -0700

    Finished Balanced Parens

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,76 @@
  */
 
 function balancedParens(input){
+    let leftCurly = -1;
+    let rightCurly = -1;
+    let leftSquare = -1;
+    let rightSquare = -1;
+    let leftParen = -1;
+    let rightParen = -1;
 
+    let leftCCount = 0;
+    let rightCCount = 0
+    let leftSCount = 0;
+    let rightSCount = 0;
+    let leftPCount = 0;
+    let rightPCount = 0;
+
+    let result = true;
+
+    for(let i = 0; i < input.length; i++) {
+        switch (input[i]) {
+            case "{":
+              if(leftCurly === -1)
+                leftCurly = i;
+              leftCCount ++;
+              break;
+            case "}":
+              rightCurly = i;
+              rightCCount ++;
+              break;
+            case "[":
+              if(leftSquare === -1)
+                leftSquare = i;
+              leftSCount++;
+              break;
+            case "]":
+              rightSquare = i;
+              rightSCount++;
+              break;
+            case "(":
+              if(leftParen === -1)
+                leftParen = i;
+              leftPCount++;
+              break;
+            case ")":
+              rightParen = i;
+              rightPCount++;
+              break;
+        }
+        console.log(leftPCount, " - ", rightPCount);
+        if(result) {
+            if(leftCCount === rightCCount && leftCurly !== -1){
+                console.log("TESTING -", input.slice(leftCurly+1, rightCurly));
+                result = balancedParens(input.slice(leftCurly+1, rightCurly));
+                i = rightCurly;
+                rightCurly = -1;
+                leftCurly = -1;
+            } else if (leftSCount === rightSCount && leftSquare !== -1) {
+              console.log("TESTING SQUARE-", input.slice(leftSquare+1, rightSquare));
+                result = balancedParens(input.slice(leftSquare+1, rightSquare));
+                i = rightSquare;
+                rightSquare = -1;
+                leftSquare = -1;
+            } else if (leftPCount === rightPCount && leftParen !== -1){
+                console.log("TESTING PAREN -", input.slice(leftParen+1, rightParen));
+                result = balancedParens(input.slice(leftParen+1, rightParen));
+                i = rightParen;
+                rightParen = -1;
+                leftParen = -1;
+            }
+        }
+    }
+    return result && (leftCurly === -1 && rightCurly == -1 && leftSquare === -1 && rightSquare === -1 && leftParen === -1 && rightParen === -1);
 }
 
 module.exports = balancedParens;
