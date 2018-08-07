963832d1f95836938ae09f8f237f563f77b24cdc
James Sieu <jamessieu@gmail.com>
2018-08-07 10:07:48 -0700

    first pass

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -25,7 +25,54 @@
  */
 
 function balancedParens(input){
+    const temp1 = input.slice(0,input.length/2).split('');
+    const array1 = [];
+    const temp2 = input.slice(input.length/2).split('').reverse();
+    const array2 = [];
 
+    if(input.length === 1) return false;
+
+    temp1.forEach(function(character){
+        if(character === '(' || character === ')' || character === '[' || character === ']' || character === '{' || character === '}'){
+            array1.push(character);
+        }
+    });
+
+    temp2.forEach(function(character){
+        if(character === '(' || character === ')' || character === '[' || character === ']' || character === '{' || character === '}'){
+            array2.push(character);
+        }
+    });
+
+    for(let x = 0; x < array1.length; x++){
+        if(array1[x] === ')' || array1[x] === ']' || array1[x] === '}')
+            return false;
+
+        if(array1[x] === '('){
+            if(array2[x] !== ')'){
+                return false;
+            }
+        }
+        if(array1[x] === '['){
+            if(array2[x] !== ']'){
+                return false;
+            }
+        }
+        if(array1[x] === '{'){
+            if(array2[x] !== '}'){
+                return false;
+            }
+        }
+    }
+    return true;
 }
+
+console.log(balancedParens('[](){}')); // true
+console.log(balancedParens('[({})]'));   // true
+console.log(balancedParens('[(]{)}')); // fals
+console.log(balancedParens('('));  // false
+ console.log(balancedParens('()')); // true
+ console.log(balancedParens(')('));  // false
+ console.log(balancedParens('(())'));  // true
 
 module.exports = balancedParens;
