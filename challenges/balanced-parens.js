46ec953ba1f4bd2cd36c4627457dcdf1e56c0467
Patrick Slagle <slaglebagel17@gmail.com>
2018-08-07 10:44:26 -0700

    failed to use recursion.

--- challenges/balanced-parens.js
+++ challenges/balanced-parens.js
@@ -3,28 +3,53 @@
  * the parentheses are balanced and false otherwise.
  *
  * Example:
- *   balancedParens('(');  // false
- *   balancedParens('()'); // true
- *   balancedParens(')(');  // false
- *   balancedParens('(())');  // true
- *
- * Step 2:
- *   make your solution work for all types of brackets
- *
- * Example:
- *  balancedParens('[](){}'); // true
- *  balancedParens('[({})]');   // true
- *  balancedParens('[(]{)}'); // false
- *
- * Step 3:
- * ignore non-bracket characters
- * balancedParens(' var wow  = { yo: thisIsAwesome() }'); // true
- * balancedParens(' var hubble = function() { telescopes.awesome();'); // false
- *
- *
- */
+//  */
+// console.log(balancedParens('('));  // false
+// console.log(balancedParens('()')); // true
+// console.log(balancedParens(')('));  // false
+console.log(balancedParens('(())'));  // true
+// // Step 2:
+// //   make your solution work for all types of brackets
+// // Example:
+// console.log(balancedParens('[](){}')); // true
+// console.log(balancedParens('[({})]'));   // true
+// console.log(balancedParens('[(]{)}')); // false
+// // Step 3:
+// // ignore non-bracket characters
+// console.log(balancedParens(' var wow  = { yo: thisIsAwesome() }')); // true
+// console.log(balancedParens(' var hubble = function() { telescopes.awesome();')); // false
+
 
 function balancedParens(input){
+    let clearedArr = input.replace(/\w|[:.;= ]/g, '')
+    const oppositeObj = {
+        '{': '}',
+        '}': '{',
+        '[': ']',
+        ']': '[',
+        '(': ')',
+        ')': '('
+    }
+    
+    while (clearedArr.length !== 0){
+        const currentBracket = clearedArr[0];
+        clearedArr = clearedArr.slice(1);  
+        search(currentBracket, clearedArr)
+    }
+
+    function search(bracket, remainingArray){
+        let counter = 0;
+        const oppositeBracket = oppositeObj[bracket]; 
+        for (let j = 0; j < remainingArray.length; j++){
+            if (remainingArray[j] === bracket) {
+                return search(remainingArray[j], remainingArray.slice(j+1))
+            }
+            if (remainingArray[j] === oppositeBracket) {
+                return true;
+            }
+        }
+        return false; 
+    }
 
 }
 
