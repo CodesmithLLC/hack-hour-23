231882b1a06ccc1338df42281a7cf73bcfa176ab
Patrick Slagle <slaglebagel17@gmail.com>
2018-08-08 09:32:35 -0700

    finished hack hour

--- challenges/roman-numeral.js
+++ challenges/roman-numeral.js
@@ -18,7 +18,18 @@
  */
 
 function romanNumeral(n) {
+    let romanString = ''; 
+    const romanNumbers = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+    const romanLetters = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
 
+    for (let i = 0; i < romanNumbers.length; i += 1){
+        while(n >= romanNumbers[i]){
+            romanString += romanLetters[i];
+            n -= romanNumbers[i]; 
+        } 
+    }
+    return romanString
 }
+
 
 module.exports = romanNumeral;
