ea445996ff01c32679183c0eb4500800ca8f586f
gerretkubota <gtkubota@cpp.edu>
2018-08-11 09:44:10 -0700

    works?

--- challenges/bin-to-dec.js
+++ challenges/bin-to-dec.js
@@ -14,7 +14,27 @@
  */
 
 function binToDec(binary) {
-
+  // 0 -> 0
+  // 01 -> 1
+  // 10 -> 2
+  // 11 -> 3
+  // loop through the number of digits in the binary
+    // check if the digit is whether a 0 or 1
+    // the ith index should be used nth power of 2
+    // maybe reverse the string first?
+  let reversed = binary.split('');
+  reversed = reversed.reverse();
+  reversed = reversed.join('');
+  let total = 0;
+  for(let i = 0; i < binary.length; i++){
+    if(reversed.charAt(i) === '1'){
+      total += Math.pow(2, i);
+    }
+  }
+  return total.toString();
 }
+
+// console.log(binToDec('0101'));
+
 
 module.exports = binToDec;
