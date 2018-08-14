a0e7ec298edcde9d5f47b4f569b258dcb2a8a184
Eddie <choi.eddie93@gmail.com>
2018-08-14 09:56:35 -0700

    submit logic for HH

--- challenges/match-word.js
+++ challenges/match-word.js
@@ -8,10 +8,32 @@
 // matchWord('IF()()fi[]');  -> true        (should be case-insensitive)
 // matchWord('for__if__rof__fi');  -> false     not properly closed. like ( [) ] 
 // matchWord('%%$@$while  try ! yrt  for if_fi rof #*#  elihw');  -> true
+                while try yrt for if fi rof elihw
+                while for if fi rof try yrt elihw
 // matchWord('');  -> true
 
 function matchWord(str) {
+    // remove all non characters in the given string
+    // 
+    // move to first character
+    // store till first non-char in an array
+    // 
+    let stack = [];
+    let current = "";
+    let newStr = str.toLowerCase();
 
+    function isChar(c) {
+        if (c > 97 && c < 122) {return true}
+        else {return false}; // doesnt work in javascript, must find some way to convert the char into an int so that the ascii comparison can be made
+    }
+    for (let i = 0; i < str.length; i++) {
+        if (!isChar(str[i])) {
+            i++;
+        } else while (isChar(str[i])) {
+            // grab first word, reverse, check if it exists in the stack, push if it does not exist, pop if it does exist.
+        }
+        // check to see if the stack is empty. if it is return true, else return false.
+    }
 }
 
 module.exports = matchWord;
