c8417682d3526fe14de26c0dde1c0d8eadd377c5
Samuel Hickey <sam.august@gmail.com>
2018-08-15 12:56:36 -0400

    ugly

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,26 @@
  */
 
 function highestProduct(array) {
+  let highestThreePositivesProduct;
+  let highestThreeNegativesProduct;
   if (!Array.isArray(array) || array.length < 3) return 0;
-  return array.sort((a,b) => b - a).slice(0,3).reduce( (a,b) => a * b, 1)
+
+  const highestThreePositives = array.filter( num => num > 0).sort((a,b) => b - a).slice(0,3)
+
+  const highestThreeNegatives = array.filter( num => num < 0).sort((a,b) => b - a).slice(0,3);
+
+  if (highestThreePositives.length >= 3) {
+    highestThreePositivesProduct = highestThreePositives.reduce((a,b)=>a*b, 1);
+  }
+
+  if (highestThreeNegatives.length >= 3) {
+    highestThreeNegativesProduct = highestThreeNegatives.reduce((a,b)=>a*b, 1);
+  }
+
+  const highestPositive = highestThreePositives[0];
+  const highestTwoNegativesProduct = array.filter( num => num < 0).slice(0,2).reduce((a,b) => a*b);
+
+  return Math.max(highestThreePositivesProduct || -Infinity, (highestPositive * highestTwoNegativesProduct) || -Infinity, highestThreeNegativesProduct || -Infinity);
 }
 
 
