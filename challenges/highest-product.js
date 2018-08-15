f5f5df70bb7eac0209d24ab475150885d1e19bd5
James Sieu <jamessieu@gmail.com>
2018-08-15 09:26:51 -0700

    highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,14 @@
  */
 
 function highestProduct(array) {
+  let product = 0;
 
+  // sort array of numbers
+  // return the product of the 3 highest numbers
+  array.sort((a, b) => a - b);
+
+  return array[array.length - 1] * array[array.length - 2] * array[array.length - 3];
 }
-
+// console.log(highestProduct([4, 3, 100, 2, 7, 9, 2]));
 
 module.exports = highestProduct;
