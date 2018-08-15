1886c9d4921eb2ea95d76c9c811aed90abd76f63
TheBlueJ <jwrightbluj@gmail.com>
2018-08-15 09:59:36 -0700

    Finished highest-product challenge

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,66 @@
  */
 
 function highestProduct(array) {
+    let result = 0;
+    let arr = array;
+    if(arr.length < 3){ return undefined };
+
+    function findHighest(array){
+        let indexOfHighest = 0;
+        for(let i in array){
+            if(array[i] > array[indexOfHighest]){
+                indexOfHighest = i;
+            }
+        }
+        return indexOfHighest;
+    }
+
+    let f1 = findHighest(arr);
+    result = arr[f1];
+    arr.splice(f1, 1)
+    let f2 = findHighest(arr);
+    result *= arr[f2];
+    arr.splice(f2, 1);
+    let f3 = findHighest(arr);
+    result *= arr[f3];
+
+    return result;
 
 }
+
+/*
+function highestProductOfN(array, n) {
+    let result = 0;
+    let arr = array;
+    if(arr.length < n){ return undefined };
+
+    function findHighest(array){
+        let indexOfHighest = 0;
+        for(let i in array){
+            if(array[i] > array[indexOfHighest]){
+                indexOfHighest = i;
+            }
+        }
+        return indexOfHighest;
+    }
+
+    for (let i = 0; i<n; i++) {
+      let factor = findHighest(arr);
+      console.log(arr[factor]);
+      if(result == 0){result = arr[factor]}
+      else {result *= arr[factor]}
+      arr.splice(factor, 1);
+    }
+
+    return result;
+
+}
+*/
+
+
+
+
+highestProduct([1, 2, 3])
 
 
 module.exports = highestProduct;
