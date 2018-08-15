5a281c833f468ceb82a7091cc42a1d5d9314cf53
seano <sth.xxx@gmail.com>
2018-08-15 09:28:12 -0700

    searchin my car, lookin' for the product... -ice cube

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,12 @@
  */
 
 function highestProduct(array) {
-
+  let workable = array.slice();
+  let producers = []
+  while(producers.length < 3){
+    producers.push(workable.splice(workable.indexOf(Math.max(...workable)),1));
+  }
+  return producers.reduce((acc,curr)=> acc *= curr);
 }
-
 
 module.exports = highestProduct;
