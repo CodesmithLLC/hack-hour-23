40d1024236189ad300631f5039755a78ae47a6ac
seano <sth.xxx@gmail.com>
2018-08-15 09:42:07 -0700

    better

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,12 +3,22 @@
  */
 
 function highestProduct(array) {
-  let workable = array.slice();
-  let producers = []
-  while(producers.length < 3){
-    producers.push(workable.splice(workable.indexOf(Math.max(...workable)),1));
+  if(!Array.isArray(array) || array.length<3){
+    return 0;
   }
+  let workable = array.slice();
+  let allPositive = workable.map(x => Math.abs(x));
+  let producers = [];
+  while(producers.length < 2){
+    let absMaxIdx = allPositive.indexOf(Math.max(...allPositive));
+    producers.push(Math.abs(workable.splice(absMaxIdx,1)));
+    allPositive.splice(absMaxIdx,1);
+  }
+  producers.push(Math.abs(workable.splice(workable.indexOf(Math.max(...workable)),1)));
+  
   return producers.reduce((acc,curr)=> acc *= curr);
 }
+
+console.log(highestProduct([-3,-3,-3,1,1,1,1,2,2]))
 
 module.exports = highestProduct;
