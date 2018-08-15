d7112f14c6ee7596b718edc783d73eb90be02158
Eddie <choi.eddie93@gmail.com>
2018-08-15 09:28:04 -0700

    submit for Hack hour

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,15 @@
  */
 
 function highestProduct(array) {
-
+    let prod = 1;
+    if (array.length < 3){
+        return "invalid";
+    }
+    let newArr = array.sort();
+    for (let i = 0; i <= 2; i++) {
+        prod *= newArr[newArr.length - i - 1];
+    }
+    return prod;
 }
-
 
 module.exports = highestProduct;
