03b9cc35f908f021409292890b6d9e4349a3d971
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-08-15 09:52:11 -0700

    checks for array input and handles neg numbers

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,10 @@
  */
 
 function highestProduct(array) {
-  let product = 0;
+  if (!Array.isArray(array)) return 0;
   if (array.length < 3) return 0;
+  let product = array[0] * array[1] * array[2];
+
   let i = 0, j = 0, k = 0;
   while (i < array.length - 2) {
     j = i + 1;
@@ -25,6 +27,9 @@ function highestProduct(array) {
 
 // console.log(highestProduct([1,2,3,4,5])); // => 60?
 // console.log(highestProduct([1,4,2,3,5])); // => 60?
+console.log(highestProduct([1,4,5]));
+console.log(highestProduct([-1,-4,-5]));
+
 
 
 
