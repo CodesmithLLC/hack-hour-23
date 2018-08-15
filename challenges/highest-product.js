ba95e9111a141966cfbf37d732324c2b9785e764
James Sieu <jamessieu@gmail.com>
2018-08-15 09:57:44 -0700

    hmmmm

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -7,20 +7,24 @@ function highestProduct(array) {
   let product2 = 0;
 
   // handle array less than 3 integers and input of not array
-  if (array.length < 3) return 0;
-  // if (!Array.isArray(array)) return 0;
+  if (array.length < 3) {
+    return 0;
+  }
+  if (!Array.isArray(array)) {
+    return 0;
+  }
 
-  // // sort array of numbers
-  // array.sort((a, b) => a - b);
+  // sort array of numbers
+  array.sort((a, b) => a - b);
 
-  // // if there are two negative numbers
-  // if (array[0] < 0 && array[1] < 0) {
-  //   product1 = array[0] * array[1] * array[array.length - 1];
-  // }
-  // // else return product of 3 highest positive integers
-  // product2 = array[array.length - 1] * array[array.length - 2] * array[array.length - 3];
+  // if there are two negative numbers
+  if (array[0] < 0 && array[1] < 0) {
+    product1 = array[0] * array[1] * array[array.length - 1];
+  }
+  // else return product of 3 highest positive integers
+  product2 = array[array.length - 1] * array[array.length - 2] * array[array.length - 3];
 
-  // return product1 > product2 ? product1 : product2;
+  return product1 > product2 ? product1 : product2;
 }
 // console.log(highestProduct([-1]));
 
