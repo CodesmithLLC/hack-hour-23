44bd005940884e0c3523d2dcdd372c4f3e13ecd3
andwong91 <andwong91@gmail.com>
2018-08-15 09:32:54 -0700

    Highest Product Completed

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,19 @@
  */
 
 function highestProduct(array) {
-
+  // edge case: check if array is an array
+  if (!Array.isArray(array)) return undefined;
+  // edge case: less than 3 numbers
+  if (array.length < 3) return undefined;
+  // input: array of integers
+  const result = array;
+  const productArr = [];
+  // loop: find index of max value 3 times and splice each time
+  for (let i = 0; i < 3; i++) {
+    productArr.push(...result.splice(result.indexOf(Math.max(...result)), 1));
+  }
+  // output: highest product of three numbers in the input array
+  return productArr[0] * productArr[1] * productArr[2];
 }
-
 
 module.exports = highestProduct;
