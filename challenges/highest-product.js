86ed877e03ce099c7bd7719794b7d1a039f85969
Greg <gregshamalta@yahoo.com>
2018-08-15 11:08:52 -0700

    highest update

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,6 +3,7 @@
  */
 
 function highestProduct(array) {
+  if (array.length > 3) {
   const sortedArr = [array.shift()];
   array.forEach(num => {
     let index = sortedArr.length - 1;
@@ -16,6 +17,8 @@ function highestProduct(array) {
     sortedArr.splice(index + 1, 0, num)
   })
   return sortedArr[0] + sortedArr[1] + sortedArr[2]
+  }
+  return array.reduce((acc, cur) => acc = acc + cur)
 }
 
 module.exports = highestProduct;
