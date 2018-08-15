bd845579f8c00ae9995e1350919d56301e7cf37c
James Sieu <jamessieu@gmail.com>
2018-08-15 09:39:37 -0700

    boundary check

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,14 +3,25 @@
  */
 
 function highestProduct(array) {
-  let product = 0;
+  let product1 = 0;
+  let product2 = 0;
+
+  // handle array less than 3 integers and input of not array
+  if (array.length < 3) return 0;
+  if (!Array.isArray(array)) return 0;
 
   // sort array of numbers
-  // return the product of the 3 highest numbers
   array.sort((a, b) => a - b);
 
-  return array[array.length - 1] * array[array.length - 2] * array[array.length - 3];
+  // if there are two negative numbers
+  if (array[0] < 0 && array[1] < 0) {
+    product1 = array[0] * array[1] * array[array.length - 1];
+  }
+  // else return product of 3 highest positive integers
+  product2 = array[array.length - 1] * array[array.length - 2] * array[array.length - 3];
+
+  return product1 > product2 ? product1 : product2;
 }
-// console.log(highestProduct([4, 3, 100, 2, 7, 9, 2]));
+// console.log(highestProduct([-3, -2, -1, -5, 2, 3, 4, 7]));
 
 module.exports = highestProduct;
