e62a339da54ac85417a9f2b6c7782f96537379f7
Colin Roemer <colin.roemer@gmail.com>
2018-08-15 09:32:22 -0700

    second attempt highest product

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -3,8 +3,17 @@
  */
 
 function highestProduct(array) {
+  if (!Array.isArray(array) || array.length < 3) {
+    return 0;
+  } 
   array.sort((a, b) => a < b);
-  return array[0] * array[1] * array[2];
+  console.log(array);
+  if (array[array.length - 1] < 0 && array[array.length - 2]) {
+    array[array.length - 1]  = array[array.length - 1] * -1;
+    array[array.length - 2]  = array[array.length - 2] * -1;
+  }
+  array.sort((a, b) => a < b);
+  return array[0] * array[array.length - 1] * array[array.length - 1];
 }
 
 
