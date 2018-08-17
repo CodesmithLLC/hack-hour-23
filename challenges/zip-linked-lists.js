549fe31701ed41867b97b96adc975a509869de89
James Sieu <jamessieu@gmail.com>
2018-08-17 10:22:20 -0700

    zip-linked-list

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,44 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
-};
+  //loop through l1 until .next is null
+  let result = l1;
+  let temp = result;
+  // console.log(result);
+  while (result) {
+    result = result.next;
+    // console.log(result);
+    if (result.next === null) {
+      result.next = l2;
+      // console.log(result);
+      while (l2) {
+        result.next = l2;
+        result = result.next;
+        l2 = l2.next;
+        if (l2.next === null) break;
+      }
+      // console.log(result);
+      // console.log('whatup');
+      break;
+    }
+  }
 
-module.exports = {Node: Node, zip: zip};
+  // console.log(temp);
+  return temp;
+}
+
+// let node1 = new Node(1);
+// let node2 = new Node(2);
+// let node3 = new Node(3);
+// node1.next = node2;
+// node2.next = node3;
+
+// let node4 = new Node(4);
+// let node5 = new Node(5);
+// let node6 = new Node(6);
+// node4.next = node5;
+// node5.next = node6;
+
+// console.log(zip(node1, node4));
+
+module.exports = { Node: Node, zip: zip };
