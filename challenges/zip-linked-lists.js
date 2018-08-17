e575051183fa40536025b8337eaddb4c1b9f1347
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-08-17 09:59:42 -0700

    initial commit

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,6 +11,31 @@ function Node(val) {
 }
 
 function zip(l1, l2) {
+  if (!l1 && !l2) return 0;
+  else if (!l1) return l2;
+  else if (!l2) return l1;
+  else {
+    const start = l1;
+    let placeholder;
+
+    while (!l1 && !l2) {
+
+      placeholder = l1.next;      
+      l1.next = l2;
+
+      if (!placeholder) break;
+
+      l1 = placeholder;
+      placeholder = l2.next;
+      l2.next = l1;
+
+      if (!placeholder) break;
+      
+      l2 = placeholder;
+    }
+  }
+
+  return start;
 };
 
 module.exports = {Node: Node, zip: zip};
