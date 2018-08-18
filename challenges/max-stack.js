17d4541fdb0c8f4d3c7cc2deccf7a68e1fdb45a3
Colin Roemer <colin.roemer@gmail.com>
2018-08-18 09:52:50 -0700

    max stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -6,8 +6,26 @@
  * BONUS: The getMax method should retrieve the maximum value from the stack in O(1) time.
  */
 
+
 function Stack() {
-  // body...
+  this.output = [];
+  this.outputLength = 0;
 }
 
+Stack.prototype.push = (val) => {
+  this.output[this.outputLength] = val;
+  return this.outputLength += 1;
+  
+}
+
+Stack.prototype.pop = () => {
+  let lastVal = this.output[this.outputLength];
+  delete this.output[this.outputLength];
+  this.outputLength -= 1;
+  return lastVal
+}
+
+Stack.prototype.getMax = () => {
+  return this.output.sort((a,b) => a > b)[0]
+}
 module.exports = Stack;
