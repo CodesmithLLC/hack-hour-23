9bb0e06403d7a774f9a7cf6fa4209e6637151ddf
G0ldenSp00n <hayfithyan@gmail.com>
2018-08-18 10:32:40 -0700

    Finished Max-Stack

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,35 @@
  */
 
 function Stack() {
-  // body...
+  this.storage = [];
+  this.max = [];
+  this.length = 0;
+
+  this.push = this.push.bind(this);
+  this.pop = this.pop.bind(this);
+  this.getMax = this.getMax.bind(this);
 }
+
+Stack.prototype.push = function (val) {
+  this.storage[this.length++] = val;
+  if(this.max[this.max.length - 1] > val){
+    this.max.push(val);
+  } else {
+    this.max.push(val);
+    this.max.sort((a, b) => a - b);
+  }
+  return this.length;
+}
+
+Stack.prototype.pop = function () {
+  let temp = this.storage[--this.length];
+  this.storage[this.length] = undefined;
+  this.max.splice(this.max.indexOf(temp), 1);
+  return temp;
+}
+
+Stack.prototype.getMax = function () {
+  return this.max[this.max.length - 1];
+} 
 
 module.exports = Stack;
