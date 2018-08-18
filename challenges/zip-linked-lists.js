a032a8e3021cc84a3cea93243796df92be0889a2
Daniel Matuszak <danielmatuszak@Daniels-MacBook-Pro.local>
2018-08-18 13:52:00 -0700

    solved zip link

--- challenges/zip-linked-lists.js
+++ challenges/zip-linked-lists.js
@@ -11,7 +11,28 @@ function Node(val) {
 }
 //l1 and l2 are LL's
 function zip(l1, l2) {
-  //
+  if(!l1) return l2;
+  if(!l2) return l1;
+  let curr1 = l1;
+  let curr2 = l2;
+  while (curr1 && curr2) {
+    const holder = curr1.next;
+    console.log('holder',holder);
+    curr1.next = curr2;
+    curr1 = curr1.next;
+    curr2 = holder;
+    console.log('curr1', curr1);
+    console.log('curr2', curr2);
+    
+  }
+  return l1;
 };
-
+let l1 = new Node(1);
+l1.next = new Node(3);
+l1.next.next = new Node(5);
+let l2 = new Node(2);
+l2.next = new Node(4);
+l2.next.next = new Node(6);
+//console.log(l1);
+console.log(zip(l1, l2));
 module.exports = {Node: Node, zip: zip};
