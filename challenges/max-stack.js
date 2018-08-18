e2e6747fc0f3b3d6d29242a1eea934b686839d6e
gerretkubota <gtkubota@cpp.edu>
2018-08-18 09:50:48 -0700

    stack ittt

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -8,6 +8,40 @@
 
 function Stack() {
   // body...
+  // push returns new length of the stack
+  // pop should return element that was removed
+  // should return the largest value in the stack
+    // should be able to retrieve the maximum value form the stack in constant time
+  this.arr = [];
+  this.push = (value) => {
+    this.arr.push(value);
+    return this.arr.length;
+  };
+  this.pop = () => {
+    return this.arr.shift();
+  }
+  this.getMax = () => {
+    if(this.arr.length){
+      const tempArr = Object.assign([], this.arr);
+      tempArr.sort((a, b) => { return a - b});
+      console.log(tempArr);
+      return tempArr[tempArr.length - 1];
+    }
+    return undefined;
+  }
 }
+
+// let s = new Stack();
+// // console.log(stack);
+// console.log(s.push(3));
+// console.log(s.push(50));
+// console.log(s.push(2));
+// console.log(s.push(1));
+// console.log(s.push(73));
+// console.log(s.push(100));
+// console.log(s.getMax());
+// // console.log(s.pop());
+
+
 
 module.exports = Stack;
