9fa48efa2d06142d046991145267bec45b532de5
Dylan Bergstrom <contact.dylanbergstrom@gmail.com>
2018-08-18 09:55:24 -0700

    done check

--- challenges/max-stack.js
+++ challenges/max-stack.js
@@ -7,7 +7,41 @@
  */
 
 function Stack() {
-  // body...
+  this.length = 0;
+  this.stack = [];
+  this.max;
+
+  this.push = function(n) {
+    // places a value to the position of length
+    this.stack[length] = n;
+    // increments length by 1
+    this.length++
+    // sets new value at length to undefined
+    this.stack[length] = undefined;
+    //setMax
+    if (n > this.max) {
+      this.max = n;
+    }
+    // returns length
+    return length;
+  }
+
+  this.pop = function() {
+    // saves the value at the index of length - 1 to a holder variable
+    let temp = this.stack[length-1] 
+    // changes the value at the index of length to undefined
+    this.stack[length-1] = undefined;
+    // decrements length
+    this.length--
+
+    return temp;
+  }
+
+  this.getMax = function() {
+    if (this.length > 0) return this.max;
+    else return undefined;
+  }
+
 }
 
 module.exports = Stack;
