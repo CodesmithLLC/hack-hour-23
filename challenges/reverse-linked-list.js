f83d2ac1b02292b47a55f5ad05cf845639c80d0d
Patrick Slagle <slaglebagel17@gmail.com>
2018-08-21 11:02:10 -0700

    finished reverse linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,39 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
-
+    if (!head.next) return head; 
+    if (!head.next.next) {
+        let temp = head
+        head = head.next
+        head.next = temp; 
+        head.next.next = null; 
+        return head; 
+    }
+    let first = head.next
+    let second = head.next.next;
+    head.next = null; 
+    while (first){
+        first.next = head; 
+        head = first; 
+        first = second;
+        if (second) second = second.next
+    }
+    return head; 
 }
+
+
+const left1 = new Node(1)
+const left2 = new Node(2)
+// const left3 = new Node(3)
+// const left4 = new Node(4)
+// const left5 = new Node(5)
+
+left1.next = left2;
+// left2.next = left3;
+// left3.next = left4;
+// left4.next = left5; 
+
+console.log('reverse it ', reverseLinkedList(left1))
+
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
