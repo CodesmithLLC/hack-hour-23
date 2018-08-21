11ccdc067ebc0408e396920fcbe25fcbf46e81d2
seano <sth.xxx@gmail.com>
2018-08-21 09:59:14 -0700

    in-place attempt..

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -13,32 +13,55 @@ function Node(value) {
     this.next = null;
 }
 
+
 function reverseLinkedList(head) {
+  if(!head) return head;
+  let agent = new Node(null);
   let current = head;
 
-  let nodes = [];
-  
-  //out of place solution
-  //get to the tail
   while(current.next){
-    nodes.push(current);
-    current = current.next;
+    let storage;
+    agent.next = current.next;
+    current.next = agent.value;
+    agent.value = current;
+    
+    if(agent.next.next){
+      storage = agent.next.next;
+      agent.next.next = agent.value;
+      agent.value = agent.next;
+      current = storage;
+    }else{
+      return agent.next;
+    }
   }
-  nodes.push(current); //push the tail on
-  let tail = current;
-
-  nodes.reverse();
-
-  for(let i=0;i<nodes.length;i++){
-    let node = nodes[i];
-    if(nodes[i+1]) node.next = nodes[i+1];
-    else nodes.next = null;
-  }
-
-  return nodes[0];
-
-
-
 }
+
+
+
+
+
+
+// function reverseLinkedList(head) {
+//   let current = head;
+
+//   let nodes = [];
+  
+//   //out of place solution
+//   //get to the tail
+//   while(current.next){
+//     nodes.push(current);
+//     current = current.next;
+//   }
+//   nodes.push(current); //push the tail on
+
+//   nodes.reverse();
+
+//   for(let i=0;i<nodes.length;i++){
+//     let node = nodes[i];
+//     if(nodes[i+1]) node.next = nodes[i+1];
+//     else nodes.next = null;
+//   }
+//   return nodes[0];
+// }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
