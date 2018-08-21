f2b5fbafb090a7757714d8563329b610c9d1d7d3
Dylan Bergstrom <contact.dylanbergstrom@gmail.com>
2018-08-21 09:50:38 -0700

    reversed linked list

--- challenges/reverse-linked-list.js
+++ challenges/reverse-linked-list.js
@@ -14,7 +14,19 @@ function Node(value) {
 }
 
 function reverseLinkedList(head) {
+    // set pointers
+    let curr = head;
+    let next = null;
+    let prev = null;
 
+
+    while (curr.value != null) { //while current is not pointing at a null value
+    next = curr.next;             // next moves to current's next value
+    curr.next = prev.value;       // current's next value becomes previous' value (where the reversing actually takes place)
+    prev = curr.value;            // previous becomes current's value
+    curr.value = next.value;      // current moves down the list to next's value
+    }
+    head = prev.value;            // Once current hits null, we drop out of while loop and then reset head to the previous value;
 }
 
 module.exports = {Node: Node, reverseLinkedList: reverseLinkedList};
