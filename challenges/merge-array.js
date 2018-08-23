8222ffc36873b420ab301e87308f8cabdda89ec7
James Sieu <jamessieu@gmail.com>
2018-08-23 09:31:10 -0700

    merge-array

--- challenges/merge-array.js
+++ challenges/merge-array.js
@@ -14,7 +14,16 @@
  */
 
 function mergeArrays(arr1, arr2) {
+  const result = arr1.concat(arr2);
 
+  result.sort((a, b) => a - b);
+
+  return result;
 }
+
+// var my_array = [3,4,6,10,11,15,21];
+// var another_array = [1,5,8,12,14,19];
+
+// console.log(mergeArrays(my_array, another_array));
 
 module.exports = mergeArrays;
