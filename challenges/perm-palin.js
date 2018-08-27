a0641a3f6584a531b62d7ca695e6bdf2c98802c4
G0ldenSp00n <hayfithyan@gmail.com>
2018-08-27 09:43:04 -0700

    Finished Perm Palin

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,19 @@
  */
 
 function permPalin(str) {
-	
+    if(str.length === 1) return true;
+
+    let permTable = {};
+
+    for(let i = 0; i < str.length; i++) {
+        if(!permTable[str[i]]) {
+            permTable[str[i]] = 1;
+        } else {
+            delete permTable[str[i]];
+        }
+    }
+
+    return Object.keys(permTable).length <= 1;
 }
 
 module.exports = permPalin;
