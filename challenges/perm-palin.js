a2c847733ffb779bc0df424c3c899284d05d8381
Colin Roemer <colin.roemer@gmail.com>
2018-08-27 09:49:43 -0700

    perm-palin done

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,24 @@
  */
 
 function permPalin(str) {
-	
+	//check if curr str is pal
+
+  //recursively remove the last letter but dont overright the MAIN str
+
+  //recursively remove first letter
+  if (str.length === 1) {
+    return true
+  } else if (str.length === 2) {
+    return false
+  } else if (str.length > 2 ) {
+      if (str === str.split('').reverse().join('')) {
+        return true
+      } else {
+        let newStrFromStart = str.split('').slice(1, str.length).join('');
+        let newStrFromStartEnd = str.split('').slice(0, str.length - 1).join('');
+        return (permPalin(newStrFromStart) || permPalin(newStrFromStartEnd))
+      }
+    }
 }
 
 module.exports = permPalin;
