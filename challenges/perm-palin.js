dbf945b0c15edc8b1c217f85c4361926482e0770
JChan <jchan@JCs-MacBook-Pro.local>
2018-08-27 10:06:07 -0700

    updated

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -12,13 +12,17 @@
 function permPalin(str) {
   if (str.length === 1) return true;
   let sorted = str.split('').sort();
-  while (sorted.length >=3) {
+  let counter = 0;
+  while (sorted.length >= 3) {
+    if (counter === 2) break;
     if (sorted[0] === sorted[1]) {
       sorted.splice(0, 2);
-    } else {
+    } else if (sorted[0] !== sorted[1]) {
       sorted.reverse();
+      counter++;
     }
   }
+  if (sorted[0] === sorted[1]) return true;
   if (sorted.length > 1) return false;
   return true;
 }
