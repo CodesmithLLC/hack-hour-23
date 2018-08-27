c7932da5dd3b54f72df60c5d96f1707377a27145
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-08-27 10:07:05 -0700

    initial commit

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,21 @@
  */
 
 function permPalin(str) {
-	
+  if (str.length <= 1) return true;
+  let chars = new Set();
+  let char;
+
+  for (let i = 0; i < str.length; i++) {
+    char = str[i];
+    if (chars.has(char)) chars.delete(char);
+    else chars.add(char);
+  }
+  return chars.size <= 1;
 }
+
+console.log(permPalin('abab')) //=> true
+console.log(permPalin('cbaba')) //=> true
+console.log(permPalin('cbac')) //=> false
+console.log(permPalin('a')) //=> true
 
 module.exports = permPalin;
