778f93a6b5c9c93fda29354461dd747ff1a37737
Brittany Miltenberger <brittany.miltenberger@gmail.com>
2018-08-27 09:51:10 -0700

    Completed Perm Palin hack hour challenge

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,44 @@
  */
 
 function permPalin(str) {
-	
+  /* GUARD CLAUSE CHECK */
+  if (str.length === 0 || typeof(str) !== "string") {
+    return 0;
+  }
+
+  // Palindrome string must have even number of grouped chars
+  // Also, palindrome string can have single unique char
+
+  const bucket = {};
+  const strArr = str.split("");
+  
+  for (let el of strArr) {
+    if (bucket[el]) {
+      bucket[el] += 1;
+    } else {
+      bucket[el] = 1;
+    }
+  }
+
+  // Check for even grouped chars in bucket
+  let oddCounter = 0;
+  for (let key in bucket) {
+    if (bucket[key] % 2 !== 0) {
+      oddCounter += 1;
+    }
+  }
+
+  if (oddCounter <= 1) {
+    return true;
+  } else {
+    return false;
+  }
+ 
 }
+
+// console.log(permPalin("abab")); // TRUE
+// console.log(permPalin("cbaba")); // TRUE
+// console.log(permPalin("cbac")); // FALSE
+// console.log(permPalin("a")); // TRUE
 
 module.exports = permPalin;
