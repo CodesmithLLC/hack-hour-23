54ff91370c9774d4fc071efa9439824f5abef907
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-08-28 10:02:10 -0700

    intial commit of stackqueer

--- challenges/stack-queue.js
+++ challenges/stack-queue.js
@@ -4,7 +4,17 @@
 
 
 function Stack() {
-
+  const vals = {};
+  return {
+    length: 0,
+    push: function(val) {
+      vals[this.length++] = val;
+    },
+    pop: function() {
+      if (this.length === 0) return -1;
+      else return vals[--this.length];
+    }
+  }
 }
 
 
@@ -14,7 +24,39 @@ function Stack() {
 
 
 function Queue() {
-
+  const firstStack = Stack();
+  const secondStack = Stack();
+  let length = 0;
+  return {
+    enqueue: function(val) {
+      if (secondStack.length > 0) {
+        for (let i = 0; i < length; i++) {
+          firstStack.push(secondStack.pop());
+        }
+      }
+      length += 1;
+      firstStack.push(val);
+    },
+    dequeue: function() {
+      if (firstStack.length > 0) {
+        for (let i = 0; i < length; i++) {
+          secondStack.push(firstStack.pop());
+        }
+      }
+      length -= 1;
+      return secondStack.pop();
+    }
+  }
 }
+
+const nq = Queue();
+
+nq.enqueue(1);
+console.log(nq.dequeue());
+nq.enqueue(2);
+nq.enqueue(3);
+
+console.log(nq.dequeue());
+console.log(nq.dequeue());
 
 module.exports = {Stack: Stack, Queue: Queue};
