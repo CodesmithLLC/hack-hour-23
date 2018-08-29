563f1a36ee500146bbb54d28a8ed05dc32884be2
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-08-29 09:38:54 -0700

    accounted for unsorted arrays

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -2,25 +2,39 @@
  *  array that sum up to the target value; return false otherwise
  */
 
+
+// for sorted arrays
+// function twoSum(arr, n) {
+//   if (!arr) return false;
+//   let i = 0;
+//   let j = arr.length - 1;
+//   let sum;
+
+//   while (i < j) {
+//     sum = arr[i] + arr[j];
+//     if (sum < n) i += 1;
+//     if (sum > n) j -= 1;
+//     if (sum === n) return true;
+//   }
+
+//   return false;
+// }
+
+// for unsorted arrays
 function twoSum(arr, n) {
   if (!arr) return false;
-  let i = 0;
-  let j = arr.length - 1;
-  let sum;
+  let hashTable = {}
 
-  while (i < j) {
-    sum = arr[i] + arr[j];
-    if (sum < n) i += 1;
-    if (sum > n) j -= 1;
-    if (sum === n) return true;
+  for (let i = 0; i < arr.length; i++) {
+    if (hashTable[n - arr[i]]) return true;
+    else hashTable[arr[i]] = arr[i];
   }
-
   return false;
 }
 
-console.log(twoSum([1,3,4,6,9], 11));
-console.log(twoSum([1,3,4,6,9], 10));
-console.log(twoSum([1,3,4,6,9], 15));
+console.log(twoSum([1,3,4,6,9], 11)); // false
+console.log(twoSum([1,3,4,6,9], 10)); // true
+console.log(twoSum([1,3,4,6,9], 15)); // true
 
 
 
