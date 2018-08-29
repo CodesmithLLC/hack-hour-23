c2ca8cfac3a3553aaa2e0161a19c86a05e8357c7
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-08-29 09:20:08 -0700

    initial commit twosum

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,7 +3,25 @@
  */
 
 function twoSum(arr, n) {
+  if (!arr) return false;
+  let i = 0;
+  let j = arr.length - 1;
+  let sum;
 
+  while (i < j) {
+    sum = arr[i] + arr[j];
+    if (sum < n) i += 1;
+    if (sum > n) j -= 1;
+    if (sum === n) return true;
+  }
+
+  return false;
 }
+
+console.log(twoSum([1,3,4,6,9], 11));
+console.log(twoSum([1,3,4,6,9], 10));
+console.log(twoSum([1,3,4,6,9], 15));
+
+
 
 module.exports = twoSum;
