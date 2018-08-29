ae0aa6ce8ce12ca76cf38a70a5892abadfb99643
TheBlueJ <jwrightbluj@gmail.com>
2018-08-29 09:45:10 -0700

    Finished two-sum

--- challenges/perm-palin.js
+++ challenges/perm-palin.js
@@ -10,7 +10,24 @@
  */
 
 function permPalin(str) {
-	
+	function allEven(str){
+        let subStrings = {};
+        for(let i of str){ !!subStrings[i] ? subStrings[i] += 1 : subStrings[i] = 1; }
+        
+        for(let i of Object.keys(subStrings)){
+            if(subStrings[i] % 2 === 1){ return false }
+        }
+        return true;
+    }
+
+    for(let i in str){
+        if(allEven(str.slice(0, i) + str.slice(i+1, str.length))){
+            return true;
+        }
+        return false;
+    }
+
+
 }
 
 module.exports = permPalin;
