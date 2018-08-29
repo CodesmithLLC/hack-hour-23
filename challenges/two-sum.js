db66848603f8c118db8127d6ad4203d10c92deea
Brittany Miltenberger <brittany.miltenberger@gmail.com>
2018-08-29 09:50:47 -0700

    Completed Two Sum Hack Hour

--- challenges/two-sum.js
+++ challenges/two-sum.js
@@ -3,6 +3,31 @@
  */
 
 function twoSum(arr, n) {
+  // Guard Clauses
+  if (!arr || !n) {
+    return 0;
+  }
+
+  if (!Array.isArray(arr)) {
+    return 0;
+  }
+
+  if (typeof(n) !== "number") {
+    return 0;
+  }
+
+  if (arr.length === 1) {
+    return false;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 1; j < arr.length; j++) {
+      if(arr[i] + arr[j] === n) {
+        return true;
+      }
+    }
+  }
+  return false;
 
 }
 
