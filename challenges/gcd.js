8f6fe670313ca444281ec4c6660cf8e147a6cdf4
Brittany Miltenberger <brittany.miltenberger@gmail.com>
2018-08-31 09:40:17 -0700

    Solution for Greatest Common Divisor Hack Hour

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,24 @@
  */
 
 function gcd(a, b) {
+  // Guard clause check
+  if ((typeof(a) !== "number") || (typeof(b) !== "number")) {
+    return 0;
+  }
 
+  if(a === b) {
+    return a;
+  }
+
+  let max = Math.max(a, b);
+
+  for (let i = max-1; i >= 1; i--){
+    if((a % i === 0) && (b % i === 0)) {
+      return i;
+    }
+  }
 }
+
+console.log(gcd(10, 9));
 
 module.exports = gcd;
