ac48e793a9de6057839811a6d5405487e3796b3e
James Sieu <jamessieu@gmail.com>
2018-08-31 09:23:53 -0700

    gcd

--- challenges/gcd.js
+++ challenges/gcd.js
@@ -8,7 +8,24 @@
  */
 
 function gcd(a, b) {
+  if(typeof a !== 'number' || typeof b !== 'number') return 0;
 
+  let greaterVal = Math.max(a,b);
+  let lesserVal;
+  
+  greaterVal === a ? lesserVal = b : lesserVal = a;
+
+  let divisor = lesserVal;
+
+  //loop through and search for divisor of both greaterVal and lesserVal starting at lesserVal
+  while(divisor !== 1) {
+    if(greaterVal % divisor === 0 && lesserVal % divisor === 0){
+      return divisor;
+    }
+    divisor -= 1;
+  }
+    return divisor; 
 }
 
+// console.log(gcd(20,8));
 module.exports = gcd;
