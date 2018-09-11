6966f4a75ee32f0bdcc2215a5254fafa4af73012
Patrick Slagle <slaglebagel17@gmail.com>
2018-09-11 10:26:19 -0700

    did add linked list

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -18,7 +18,58 @@ function Node(val) {
 }
 
 function addLinkedList(l1, l2) {
+  if (!l1 instanceof Node || !l2 instanceof Node) {
+    return 'inputs are not linked lists'
+  }
+  let sum = new Node(l1.value + l2.value)
 
+  const helper = (l1, l2) => {
+    //find the sum of the two current nodes
+    let addedValues;
+    if (l1 && l2) addedValues = l1.value + l2.value
+    else if (l1) addedValues = l1.value;
+    else if (l2) addedValues = l2.value; 
+
+    //find the last valid node of sum
+    let temp = sum;
+    while (temp.next) {
+      temp = temp.next; 
+    }
+
+    //can simply add a new node to end of sum.
+    if (addedValues < 10){
+      temp.next = new Node(addedValues);
+    } 
+    //added value is greater than 10. Add carry over to l1
+    else {
+      temp.next = new Node(addedValues % 10);
+      const remainder = Math.floor(addedValues / 10)
+      if (l1.next){
+        l1.next.value += remainder
+      }
+      else{
+        l1.next = new Node(remainder)
+      }
+    }
+    if (l1) l1 = l1.next;
+    if (l2) l2 = l2.next;
+    if (l1 || l2) helper(l1, l2)
+    return; 
+  }
+  helper(l1.next, l2.next); 
+  return sum;
 }
+
+
+
+const left = new Node(5);
+left.next = new Node(2)
+left.next.next = new Node(3);
+
+const right = new Node(2);
+right.next = new Node(3);
+right.next.next = new Node(7)
+
+console.log('here is the answer', addLinkedList(left, right));
 
 module.exports = {Node: Node, addLinkedList: addLinkedList};
--- challenges/gcd.js
+++ challenges/gcd.js
@@ -7,26 +7,45 @@
  *
  */
 
-function gcd(a, b) {
-  //edge cases. Make sure parameters are numbers, and check if they are equal
-  if (typeof a !== 'number' || typeof b !== 'number') return 'please input numbers'
-  if (a === b) return a
+//my original solution
+// function gcd(a, b) {
+//   //edge cases. Make sure parameters are numbers, and check if they are equal
+//   if (typeof a !== 'number' || typeof b !== 'number') return 'please input numbers'
+//   if (a === b) return a
 
-  //make sure a is larger than b
-  if (b > a) {
-    const temp = a; 
-    a = b;
-    b = temp; 
-  }
+//   //make sure a is larger than b
+//   if (b > a) {
+//     const temp = a; 
+//     a = b;
+//     b = temp; 
+//   }
 
-  //loop through a, starting at a/2. Since a is larger than b, 
-  //the first common divisor possibility would be a/2
-  for (let i = a/2; i > 0; i--){
-    if (a % i === 0 && b % i === 0) return i
-  }
-  return 1
+//   //loop through a, starting at a/2. Since a is larger than b, 
+//   //the first common divisor possibility would be a/2
+//   for (let i = a/2; i > 0; i--){
+//     if (a % i === 0 && b % i === 0) return i
+//   }
+//   return 1
+// }
+
+function gcd(a, b){
+  if (b === 0) return a;
+  return gcd(b, a % b);
 }
 
+//other solution
+// function gcd(a,b){
+//   while (a > 0 && b > 0){
+//     if (a < b) b = b - a;
+//     else a = a - b; 
+//   }
+//   return a || b; 
+// }
+
+
 console.log(gcd(6, 18))
+
+
+
 
 module.exports = gcd;
