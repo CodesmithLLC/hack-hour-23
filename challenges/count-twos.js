16a0c2a33a89adf43dcb60cbe9d9bcdf1c3e88ac
Colin Roemer <colin.roemer@gmail.com>
2018-09-11 09:10:41 -0700

    pushing starting point for count twos

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -2,7 +2,7 @@
 // For example: countTwos(3) should be 1 because there are no 2s in the number one. There is One 2 in the number two and no 2s in the number three.
 // Each 2 should be counted. For instance,  the number 2222 has four 2s. 
 
-// countTwos(1);  -> 0
+// (1);  -> 0
 // countTwos(3);  -> 1
 // countTwos(13);  -> 2
 // countTwos(1000);  -> 300
@@ -10,7 +10,23 @@
 
 
 function countTwos(num) {
+let output = 0;
+console.log('num', num);
+  for (var i = 0; i <= num; i++) {
+    let iArray = i.toString().split("");
+    if (iArray.includes('2')) {
+      output++;
+      console.log('iArray', iArray);
 
+      let newI = iArray.splice(iArray.indexOf(2), 1);
+      if (newI.includes('2')) {
+        output += countTwos(parseInt(newI.join('')));
+      }
+    }
+  }
+  return output
 }
+
+
 
 module.exports = countTwos;
