422e0244d40ae19fd7a73fc8f84c0aebed73afba
Patrick Slagle <slaglebagel17@gmail.com>
2018-09-11 12:23:53 -0700

    updated count-twos

--- challenges/count-twos.js
+++ challenges/count-twos.js
@@ -8,15 +8,44 @@ console.log(countTwos(13));    // -> 2
 console.log(countTwos(1000));  // -> 300
 console.log(countTwos(11420)); //  -> 4483
 
+// brute force
+// function countTwos(num) {
+//   let count = 0;
+//   for (let i = 2; i <= num; i += 1){
+//     let digits = String(i).split('');
+//     digits.forEach(digit => {
+//       if (digit.includes('2')) count +=1
+//     })
+//   }
+//   return count; 
+// }
 
-function countTwos(num) {
-  let twoCount = 0; 
-
-  for (let i = 2; i < num; i += 10){
-    twoCount++;
-    
-  }
-  return twoCount; 
+// hacky solution
+function countTwos(num){
+  // create digit array
+  const digits = num.toString().split('').reverse();
+  
+  return digits.reduce((count, strDigit, idx, arr) => {
+    // turn string into number
+    const digit = Number(strDigit);
+    // case for first digit
+    if (idx === 0) {
+      if (digit >=2) count += 1;
+    } else {
+      // use formula to add to count based on digit and index
+      count += digit * idx * Math.pow(10, idx - 1);
+      // add 10 to the index power for digits > 2
+      if (digit > 2) {
+        count += Math.pow(10, idx);
+      } else if (digit === 2){
+        // for digits === 2, add the total of all previous digits to the count
+        count += Number(arr.slice(0, idx).join('')) + 1
+      }
+    }
+    // return updated count
+    return count;
+    // include initial value of 0
+  }, 0); 
 }
 
 module.exports = countTwos;
