2de657c1271f4d561ef5335943e328f7688edc41
Patrick Slagle <slaglebagel17@gmail.com>
2018-09-12 10:51:45 -0700

    add-linked-list and superbalanced

--- challenges/add-linked-list.js
+++ challenges/add-linked-list.js
@@ -17,49 +17,72 @@ function Node(val) {
   this.next = null;
 }
 
+// my old solution
+// function addLinkedList(l1, l2) {
+//   if (!l1 instanceof Node || !l2 instanceof Node) {
+//     return 'inputs are not linked lists'
+//   }
+//   let sum = new Node(l1.value + l2.value)
+
+//   const helper = (l1, l2) => {
+//     //find the sum of the two current nodes
+//     let addedValues;
+//     if (l1 && l2) addedValues = l1.value + l2.value
+//     else if (l1) addedValues = l1.value;
+//     else if (l2) addedValues = l2.value; 
+
+//     //find the last valid node of sum
+//     let temp = sum;
+//     while (temp.next) {
+//       temp = temp.next; 
+//     }
+
+//     //can simply add a new node to end of sum.
+//     if (addedValues < 10){
+//       temp.next = new Node(addedValues);
+//     } 
+//     //added value is greater than 10. Add carry over to l1
+//     else {
+//       temp.next = new Node(addedValues % 10);
+//       const remainder = Math.floor(addedValues / 10)
+//       if (l1.next){
+//         l1.next.value += remainder
+//       }
+//       else{
+//         l1.next = new Node(remainder)
+//       }
+//     }
+//     if (l1) l1 = l1.next;
+//     if (l2) l2 = l2.next;
+//     if (l1 || l2) helper(l1, l2)
+//     return; 
+//   }
+//   helper(l1.next, l2.next); 
+//   return sum;
+// }
+
+// Codesmith solution
 function addLinkedList(l1, l2) {
-  if (!l1 instanceof Node || !l2 instanceof Node) {
-    return 'inputs are not linked lists'
-  }
-  let sum = new Node(l1.value + l2.value)
-
-  const helper = (l1, l2) => {
-    //find the sum of the two current nodes
-    let addedValues;
-    if (l1 && l2) addedValues = l1.value + l2.value
-    else if (l1) addedValues = l1.value;
-    else if (l2) addedValues = l2.value; 
-
-    //find the last valid node of sum
-    let temp = sum;
-    while (temp.next) {
-      temp = temp.next; 
+  let cur = new Node(null); // will be the returned list, val doesnt matter
+  const head = cur; // keep track of the head to ultimately return its .next
+  let sum = 0;
+  while (l1 || l2 || sum) { // we check sum in case of carryovers from end
+    if (l1) {
+      sum += l1.value;
+      l1 = l1.next;
     }
-
-    //can simply add a new node to end of sum.
-    if (addedValues < 10){
-      temp.next = new Node(addedValues);
-    } 
-    //added value is greater than 10. Add carry over to l1
-    else {
-      temp.next = new Node(addedValues % 10);
-      const remainder = Math.floor(addedValues / 10)
-      if (l1.next){
-        l1.next.value += remainder
-      }
-      else{
-        l1.next = new Node(remainder)
-      }
+    if (l2) {
+      sum += l2.value;
+      l2 = l2.next;
     }
-    if (l1) l1 = l1.next;
-    if (l2) l2 = l2.next;
-    if (l1 || l2) helper(l1, l2)
-    return; 
+    // add the new Node to the return list, modulo 10 will give us
+    // either the number itself or the number minus 10 if > 9
+    cur.next = new Node(sum % 10);
+    cur = cur.next // move to next digit in the list we're making
+    sum = sum > 9 ? 1 : 0; // start the next iteration with a sum of 1 if we need to carry a 1
   }
-  helper(l1.next, l2.next); 
-  return sum;
+  return head.next; 
 }
-
 
 
 const left = new Node(5);
--- challenges/superbalanced.js
+++ challenges/superbalanced.js
@@ -14,7 +14,38 @@ function BinaryTree(value) {
 }
 
 function superbalanced(tree) {
+  let truth = true;
 
+  // helper will find the height at each node. If any node is unbalanced, 
+  // it will change truth to false and stop running. 
+  const helper = tree => {
+    if (truth === false) return 
+    let leftHeight = 0;
+    let rightHeight = 0; 
+    if (tree.left) {
+      leftHeight = 1 + helper(tree.left)
+    }
+    if (tree.right) {
+      rightHeight = 1 + helper(tree.right)
+    }
+    if (Math.abs(leftHeight - rightHeight) > 1) truth = false; 
+    return Math.max(leftHeight, rightHeight)
+  }
+  helper(tree)
+  return truth; 
 }
+
+
+const tree = new BinaryTree(12)
+tree.left = new BinaryTree(8)
+tree.left.left = new BinaryTree(5)
+tree.left.right = new BinaryTree(11)
+tree.left.left.left = new BinaryTree(4)
+tree.left.left.left.left = new BinaryTree(3)
+tree.left.right.right = new BinaryTree(6)
+tree.right = new BinaryTree(18)
+tree.right.left = new BinaryTree(17)
+
+console.log(superbalanced(tree))
 
 module.exports = {BinaryTree: BinaryTree, superbalanced: superbalanced};
