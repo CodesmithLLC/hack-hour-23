330e22fc1a005eb301550130b47d19161aab72a7
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-09-13 09:50:36 -0700

    initial commit of repeatNumber

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,21 @@
  */
 
 function repeatNumbers(array) {
-
+  for (let i = 0, j = 0; i < array.length; i++) {
+    if (i !== array[i] - 1) { // already in correct spot otherwise
+      j = array[array[i] - 1];
+      if (array[array[i] - 1] === array[i]) return array[i]; // duplicate number found
+      array[array[i] - 1] = array[i];
+      array[i] = j;
+    }
+  }
+  return array[array.length - 1]; // duplicate number is at end of array
 }
+
+console.log(repeatNumbers([3,2,2,1]));
+console.log(repeatNumbers([2,3,2,1]));
+console.log(repeatNumbers([2,3,1,2]));
+console.log(repeatNumbers([1,2,2,3]));
+
 
 module.exports = repeatNumbers;
