84793637258811463e7c4c48e765ae8253c3eb79
Andy Ahn <andy@Andys-MacBook-Pro.local>
2018-09-14 09:18:00 -0700

    repeat

--- challenges/repeat-number.js
+++ challenges/repeat-number.js
@@ -11,7 +11,20 @@
  */
 
 function repeatNumbers(array) {
+  const obj = {}
+  
+  let i = 0
 
+  while(i < array.length){
+    if(array[i] in obj){
+      console.log("exist!!!")
+      break;
+    }
+    obj[array[i]] = i;
+    i++
+  }
 }
 
 module.exports = repeatNumbers;
+
+console.log(repeatNumbers([1, 2, 3, 4, 5, 2]))
