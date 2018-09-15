b75c55fa43ba091d2d4ff394df93ee2a2b921175
GodofCookery <leong.bryan.c@gmail.com>
2018-09-15 10:29:12 -0700

    pseudo approach

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -9,6 +9,11 @@
  * Do not assume the ranges are in order
  */
 
+ // input array of arrays with rangees that need to be condensed.
+ // sub-arrays are not in order, but need to check each array for index[0] value, on with lowest start value as the first subarray.
+ // check each other subarray for the next value that is greater than index[1] of initial sub-array. 
+ // slice method and push method to replace numbers within the index[1] if needed.
+
 
 function mergeRanges(array) {
 
