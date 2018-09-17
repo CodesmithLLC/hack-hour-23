de2e5eb43c7bf019d1c7ae05b431d12da704de07
SelcukAtes <sselcukates@gmail.com>
2018-09-17 09:07:39 -0700

    not done merge-ranges.js

--- challenges/merge-ranges.js
+++ challenges/merge-ranges.js
@@ -11,7 +11,20 @@
 
 
 function mergeRanges(array) {
-
+    //looping thr all number and incremnt and put into new
+    //sort based on the first number in the array.
+    const rangesSorted = array.sort(function(a, b) { 
+        return a[0] > b[0] ? 1 : -1;
+    });
+    //sort thr all range than find
+    //create a new array to store new all value
+    const myArray = [];
+    for(let i = 0; i < rangesSorted.length; i++) {
+        console.log(rangesSorted[i][0])
+        console.log(rangesSorted[i][1])
+        
+       
+    }       
 }
-
+mergeRanges([[0, 1], [3, 5], [4, 8], [10, 12], [9, 10]])
 module.exports = mergeRanges;
