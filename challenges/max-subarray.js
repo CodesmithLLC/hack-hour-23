4c5f5939f1edb174ef378bd34dbb6836afee16bc
andwong91 <andwong91@gmail.com>
2018-09-18 09:29:28 -0700

    max subarray completed

--- challenges/max-subarray.js
+++ challenges/max-subarray.js
@@ -8,7 +8,27 @@
  */
 
 function maxSubarray(arr) {
-
+  if (!Array.isArray(arr)) return undefined;
+  if (arr.length === 0) return 0;
+  if (arr.length === 1) return arr[0];
+  const array = JSON.parse(JSON.stringify(arr));
+  let max = -Infinity;
+  let sum = 0;
+  while (array.length > 0) {
+    if (array.length === 1 && array[0] > max) {
+      max = array[0];
+      array.pop();
+    } else {
+      sum = array.reduce((acc, cur) => acc + cur);
+      if (sum > max) max = sum;
+      if (array[0] > array[array.length - 1]) {
+        array.pop();
+      } else {
+        array.shift();
+      } 
+    } 
+  }
+  return max;
 }
 
 module.exports = maxSubarray;
