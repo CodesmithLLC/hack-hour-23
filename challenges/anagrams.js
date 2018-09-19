c3229104ca700a45c5c48a14974c8129519c7631
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-09-19 09:52:27 -0700

    initial commit anagrams

--- challenges/anagrams.js
+++ challenges/anagrams.js
@@ -12,8 +12,25 @@
   * console.log(result); // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
   */
 
-function anagrams(string) {
+function anagrams(str, index = 0, resultArray = []) {
+  if (!str || index >= str.length) return resultArray;
+  else if (typeof str === 'string') str = str.split("");
+    
+  for (let i = index; i < str.length; i++)
+    resultArray.push(ToggleLetters(str, index, i));
 
+  return anagrams(str, index + 1, resultArray);
 }
+
+function ToggleLetters(str, index1, index2) {
+  if (index1 != index2) {
+    let temp = str[index1];
+    str[index1] = str[index2];
+    str[index2] = temp;
+  }
+  return str.join("");
+}
+
+console.log(anagrams('abc'));
 
 module.exports = anagrams;
