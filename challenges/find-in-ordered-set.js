a1e6cd2d0efdc14fb0f1df305e2b0f85a5d3d78a
Patrick Slagle <slaglebagel17@gmail.com>
2018-09-22 10:18:03 -0700

    find in ordered set

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -2,16 +2,18 @@
 How quickly could we check if a given integer is in the array?
 Assume the array is sorted.
 You can do better than O(n) time
+*/
 
 var nums = [1, 4, 6, 7, 9, 17, 45]
-findInOrderedSet(nums, 4);  -> true
-findInOrderedSet(nums, 2);  -> false
+console.log(findInOrderedSet(nums, 4));  //-> true
+console.log(findInOrderedSet(nums, 2));  //-> false
 
- */
+
 
 
 function findInOrderedSet(arr, target) {
-
+  const set = new Set(arr)
+  return this.state.has(target)
 }
 
 
