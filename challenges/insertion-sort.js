2b800aeb9d0a7e495f24f4e31a87539a7d9919c1
Brittany Miltenberger <brittany.miltenberger@gmail.com>
2018-09-24 09:40:54 -0700

    Completed Insertion Sort Hack Hour Challenge

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -6,6 +6,30 @@
 
 function insertionSort(array) {
 
+  // Guard clauses
+  if (!Array.isArray(array)) {
+    return 0;
+  }
+
+  if (array.length <= 1) {
+    return 0;
+  }
+
+  let i = 1;
+  while (i < array.length) {
+    let j = i;
+    while (( j > 0) && (array[j-1] > array[j])) {
+      let temp;
+      temp = array[j];
+      array[j] = array[j-1];
+      array[j-1] = temp;
+      j = j - 1;
+    }
+    i = i + 1;
+  }
+  return array;
 }
+
+console.log(insertionSort([8, 2, 4]));
 
 module.exports = insertionSort;
