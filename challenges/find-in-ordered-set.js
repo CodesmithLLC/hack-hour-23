e64fe46c50d246654c37f4baf99e28281907d49c
andwong91 <andwong91@gmail.com>
2018-09-24 09:37:58 -0700

    completed insertion sort

--- challenges/find-in-ordered-set.js
+++ challenges/find-in-ordered-set.js
@@ -10,9 +10,26 @@ findInOrderedSet(nums, 2);  -> false
  */
 
 
-function findInOrderedSet(arr, target) {
-
+function findInOrderedSet(arr, target, isMatch = [false]) {
+  console.log(`arr is ${arr}`);
+  // guard clauses
+  if (typeof target !== 'number') return false;
+  if (!Array.isArray(arr)) return false;
+  // recursion base case
+  if (arr.length === 1) {
+    if (arr[0] === target) {
+      isMatch[0] = true;
+      return;
+    }
+    else return;
+  }
+  findInOrderedSet(arr.slice(0, Math.ceil(arr.length / 2)), target, isMatch);
+  findInOrderedSet(arr.slice(Math.ceil(arr.length / 2), arr.length), target, isMatch);
+  return isMatch[0];
 }
+
+console.log(findInOrderedSet([1, 4, 6, 7, 9, 17, 45], 4));
+
 
 
 module.exports = findInOrderedSet;
--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -3,9 +3,20 @@
 // https://en.wikipedia.org/wiki/Insertion_sort
 
 
-
 function insertionSort(array) {
-
+  // guard clauses
+  if (!Array.isArray(array)) return undefined;
+  if (array.length === 1) return array;
+  const result = JSON.parse(JSON.stringify(array));
+  result.forEach((num, index) => {
+    for (let i = 0; i < index; i++) {
+      if (num < result[i]) {
+        result.splice(i, 0, result.splice(index, 1)[0]);
+        break;
+      }
+    }
+  });
+  return result;
 }
 
 module.exports = insertionSort;
