fba02875b3846c54153ec2680834ff84b04b79d9
SelcukAtes <sselcukates@gmail.com>
2018-09-25 09:53:44 -0700

    solved with bitwise opr unique-number.js

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -10,7 +10,20 @@
  *
  */
 function uniqueNumber(array) {
-
+   //BitWise Operation using XOR 
+   //take a look at how underthe bitwise working when you have a time 
+   // and also hackour solution
+        var result = 0;
+        for (var i = 0; i < array.length; i++) {
+          
+          result ^= array[i]
+          
+        }
+      
+      return result;
+      
+      
 }
 
+uniqueNumber([1,2,1,3,3,2,4])
 module.exports = uniqueNumber;
