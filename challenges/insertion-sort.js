7b429611ff10614a5efd79b758156c30481abc0d
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-09-25 10:04:43 -0700

    untested insertion sort

--- challenges/insertion-sort.js
+++ challenges/insertion-sort.js
@@ -5,7 +5,15 @@
 
 
 function insertionSort(array) {
-
+  for (let i = 1; i < array.length; i++) {
+    let temp = array[i];
+    let j
+    for (let j = i - 1; j >= 0 && array[j] > temp; j -= 1) {
+      array[j + 1] = array[j];
+    }
+    array[j + 1] = temp;
+  }
+  return array;
 }
 
 module.exports = insertionSort;
