a30c41597868e6d1a7a89fef7a8963fcec79f92d
andwong91 <andwong91@gmail.com>
2018-09-25 09:47:44 -0700

    unique number completed

--- challenges/unique-number.js
+++ challenges/unique-number.js
@@ -3,14 +3,16 @@
  * except for one integer that will appear exactly once. Return the integer that appears once.
  *
  * uniqueNumber([1,2,1,3,3]); -> 2
- *
+ * 
  * BONUS:
  * Complete the challenge in O(n) time
  * Complete the challenge in O(1) space
  *
  */
 function uniqueNumber(array) {
-
+  if (!Array.isArray(array)) return undefined;
+  return array.reduce((acc, cur) => acc ^ cur);
 }
 
+console.log(uniqueNumber([1, 2, 1, 3, 3, 5, 5]));
 module.exports = uniqueNumber;
