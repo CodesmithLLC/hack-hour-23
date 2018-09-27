bef6b3bdf9ab29c6520464aa12ca57b82acfb5b9
Colin Roemer <colin.roemer@gmail.com>
2018-09-27 10:10:04 -0700

    doubly-linked-list done

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,32 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
+  const node = new Node(val);
   
+  if (!this.head) {
+    this.head = node;
+  }  else {
+      node.prev = this.tail;
+      this.tail.next = node;
+  }
+
+  this.tail = node;
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let curr = this.head;
+  while ( curr.val !== this.tail.val ) {
+    if (curr.val === val) {
+      curr.next.prev = curr.prev;
+      curr.prev.next = curr.next;
+      break
+    }
+    curr = curr.next;
+  }
 };
+
 
 module.exports = LinkedList;
