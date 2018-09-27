eabcb24d041287e12055cb4b54a785abb3715a41
Joshua <jwrightbluj@gmail.com>
2018-09-27 09:40:37 -0700

    finished a bunch of hacks hour

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,32 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
+  const newNode = new Node(val);
   
+  if(!this.head) return this.head = newNode;
+  if(!!this.head && !this.tail){
+    this.tail = newNode;
+    this.head.next = newNode;
+    this.tail.prev = this.head;
+  }
+
+  newNode.prev = this.tail;
+  newNode.next = null;
+  this.tail.next = newNode;
+  this.tail = newNode;
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  const curr = this.head;
+  while(curr.val !== val){
+    curr = curr.next;
+  } 
+  //remove curr
+  curr.prev.next = curr.next;
+  curr.next.prev = curr.prev;
 };
 
 module.exports = LinkedList;
