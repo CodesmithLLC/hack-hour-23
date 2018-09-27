64f938a744e2b9a60f58b83c4b102f669e110a41
seano <sth.xxx@gmail.com>
2018-09-27 09:39:30 -0700

    i tihnk I'm done

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,40 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  newTail = new Node(val);
+  if (!this.head) {
+    this.head = newTail;
+  }
+  newTail.prev = this.tail;
+  this.tail = newTail;
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  let revIdx = this.tail;
+  let fwdIdx = this.head;
+
+  if (revIdx === fwdIdx && revIdx.val === val) {
+    this.tail = null;
+    this.head = null;
+    return;
+  }
+
+  //loop from head and tail, searchin for value
+  while (revIdx !== fwdIdx) {
+    if(revIdx.val === val){
+      revIdx.next.prev = revIdx.prev;
+      revIdx.prev.next = revIdx.next;
+    }else if(fwdIdx.val === val){
+      fwdIdx.prev.next = fwdIdx.next;
+      fwdIdx.next.prev = fwdIdx.prev;
+    }else{//not found, move idxs along
+      fwdIdx = fwdIdx.next;
+      revIdx = revIdx.prev;
+    }
+  }
 };
 
 module.exports = LinkedList;
