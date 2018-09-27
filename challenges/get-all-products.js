1ce5c5cba13bab6f42db9b00cb6cbbbd8ae70cac
andwong91 <andwong91@gmail.com>
2018-09-27 09:07:12 -0700

    get all products completed

--- challenges/get-all-products.js
+++ challenges/get-all-products.js
@@ -6,11 +6,26 @@
  * this is done via:
  * [7*3*4, 1*3*4, 1*7*4, 1*7*3]
  *
- * do not use division, becuase zero might be in the array and you cannot divide by zero
+ * do not use division, because zero might be in the array and you cannot divide by zero
  */
 
 function getAllProducts(array) {
-
+  // guard clauses
+  if (!Array.isArray(array)) return undefined;
+  if (array.length === 1) return array;
+  const result = [];
+  let product = 1;
+  for (let i = 0; i < array.length; i++) {
+    for (let j = 0; j < array.length; j++) {
+      if (i !== j) {
+        product *= array[j];
+      }
+    }
+    result.push(product);
+    product = 1;
+  }
+  return result;
 }
 
+console.log(getAllProducts([1, 7, 3, 4]));
 module.exports = getAllProducts;
