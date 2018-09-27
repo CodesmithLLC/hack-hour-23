b1e99fee10580fa56bddbde0eb338429db648ea4
andwong91 <andwong91@gmail.com>
2018-09-27 09:50:53 -0700

    doubly linked list attempt

--- challenges/doubly-linked-list.js
+++ challenges/doubly-linked-list.js
@@ -17,14 +17,57 @@ function Node(val) {
 Adds a node to the end of the list
  */
 LinkedList.prototype.add = function(val) {
-  
+  if (this.head === null && this.tail === null) {
+    const node = new Node(val);
+    this.head = node;
+    this.tail = node;
+  } else {
+    // make new node at the tail
+    this.tail.next = new Node(val);
+    // set prev equal to the current tail
+    this.tail.next.prev = this.tail;
+    // move the tail
+    this.tail = this.tail.next;
+  }
 };
 
 /*
 Removes the first node with the inputted value
  */
 LinkedList.prototype.remove = function(val) {
-  
+  if (this.head === null) return;
+  if (this.head.val === val) {
+    // move the head
+    this.head = this.head.next;
+    // remove link from previous node to the head
+    this.head.prev.next = null;
+    // remove link to previous node
+    this.head.prev = null;
+  }
+  let currentNode = this.head;
+  while (currentNode !== null) {
+    if (currentNode.val === val) {
+      // {A}      {B}      null
+      //  ^-----------------^
+      // get the next node
+      const next = currentNode.next;
+      // get the previous node
+      const prev = currentNode.prev;
+      // remove link from currentNode to next node
+      currentNode.next = null;
+      // remove link from currentNode to previous node
+      currentNode.prev = null;
+      // link previous node with currentNode.next
+      prev.next = next;
+      if (next !== null) {
+        // link next node to previous node if it isn't null
+        next.prev = prev;
+      }
+      if (prev.next === null) this.tail = prev;
+      break;
+    }
+    currentNode = currentNode.next;
+  }
 };
 
 module.exports = LinkedList;
