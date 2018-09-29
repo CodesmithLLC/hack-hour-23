197921c32747d5a97ed5f7ba6de386145dc4cca5
andwong91 <andwong91@gmail.com>
2018-09-29 09:28:36 -0700

    Bubble sort completed

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -3,9 +3,27 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 
-
+/**
+ * 
+ * @param {Object} array 
+ */
 function bubbleSort(array) {
-
+  if (!Array.isArray(array)) return undefined;
+  const sortedArr = JSON.parse(JSON.stringify(array));
+  let didSwap = true;
+  while (didSwap) {
+    didSwap = false;
+    sortedArr.forEach((num, index) => {
+      if (index !== sortedArr.length - 1) {
+        if (num > sortedArr[index + 1]) {
+          sortedArr[index] = sortedArr[index + 1];
+          sortedArr[index + 1] = num;
+          didSwap = true;
+        }
+      }
+    });
+  }
+  return sortedArr;
 }
 
 module.exports = bubbleSort;
