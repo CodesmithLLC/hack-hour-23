e5937e7df47c632cb5da690654152dcf1382d252
gerretkubota <gerretkubota@gmail.com>
2018-09-29 09:47:26 -0700

    it worksss

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -3,9 +3,27 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 
-
+/**
+ * Sorts the array in bubblesort
+ * @param {array} array 
+ */
 function bubbleSort(array) {
+ let swapped = true;
 
+  while (swapped) {
+    swapped = false;
+    for (let i = 1; i < array.length; i++) {
+      if(array[i - 1] > array[i]) {
+        const temp = array[i];
+        array[i] = array[i - 1];
+        array[i - 1] = temp;
+        swapped = true;
+      } 
+    }
+  }
+  return array;
 }
+
+console.log(bubbleSort([5,45,32,61,112,1,6]));
 
 module.exports = bubbleSort;
