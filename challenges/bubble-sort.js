0651a933180868fc3b6a2bee1264025074fece36
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-09-29 09:18:59 -0700

    initial commit of bubble sort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,26 @@
 
 
 function bubbleSort(array) {
+  
+  if (!Array.isArray(array) || array.length <= 1) return array;
 
+  for (let j = 1; j <= array.length; j++) {
+    for (let i = 0; i < array.length - j; i++) {
+      if (array[i] > array[i + 1]) {
+        let temp = array[i + 1];
+        array[i + 1] = array[i];
+        array[i] = temp;
+      }
+    }
+  }
+
+  return array;
 }
+
+console.log(bubbleSort());
+console.log(bubbleSort([]));
+console.log(bubbleSort([1]));
+console.log(bubbleSort([2,3,4,1]));
+console.log(bubbleSort([4,3,2,1]));
 
 module.exports = bubbleSort;
