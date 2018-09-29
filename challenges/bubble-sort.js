dd8c85d77d0430161fd8fb56aa4273357a8569bb
SelcukAtes <sselcukates@gmail.com>
2018-09-29 09:49:15 -0700

    two sort added insettion and bubble

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -3,9 +3,29 @@
 // https://en.wikipedia.org/wiki/Bubble_sort
 
 
+/**
+ * @param array {array}
+ */
+
+function swap(i, j, array) {
+    let temp = array[i];
+    array[i] = array[j];
+    array[j] = temp
+}
 
 function bubbleSort(array) {
 
+
+    //first Loop thr then
+    for(let i =0; i < array.length; i++) {
+
+        for(let j = i; j < array.length; j++) {
+            if(array[i] > array[j]) {
+                swap(i, j, array)
+            }
+        }
+    }
+    return array
 }
 
 module.exports = bubbleSort;
