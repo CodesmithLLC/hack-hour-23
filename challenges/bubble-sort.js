56d881b4289655fb5e45bb2c151798f214660f3a
Patrick Slagle <slaglebagel17@gmail.com>
2018-09-29 09:21:48 -0700

    bubblesort

--- challenges/bubble-sort.js
+++ challenges/bubble-sort.js
@@ -5,7 +5,20 @@
 
 
 function bubbleSort(array) {
-
+  let sortLength = array.length - 1
+  array.forEach(ele => {
+    for (let i = 0; i < sortLength; i += 1) {
+      if (array[i] > array[i+1]) {
+        const temp = array[i];
+        array[i] = array[i+1]
+        array[i+1] = temp; 
+      }
+    }
+  })
+  return array; 
 }
+
+const array = [34, 2, 10, 4, 60, 1, 10]
+console.log(bubbleSort(array))
 
 module.exports = bubbleSort;
