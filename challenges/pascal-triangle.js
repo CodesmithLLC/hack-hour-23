4868fb34cf5506c7f2ea2b8db61805d775f4fa78
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-10-05 09:33:36 -0700

    initial commit of pascal triangle

--- challenges/pascal-triangle.js
+++ challenges/pascal-triangle.js
@@ -33,7 +33,29 @@
 */
 
 function pascalTriangle(numRows) {
+  if (numRows < 1 || isNaN(numRows)) return [];
+  else if (numRows === 1) return [[1]];
 
+  const triangle = [[1], [1,1]];
+
+  if (numRows === 2) return triangle;
+
+  for (let i = 2; i < numRows; i++) {
+    const newRow = [1];
+    const lengthOfLastArray = triangle[triangle.length - 1].length;
+    const lastTriangle = triangle[triangle.length - 1];
+
+    for (let j = 0; j < lengthOfLastArray - 1; j++) {
+      newRow.push(lastTriangle[j] + lastTriangle[j + 1]);
+    }
+
+    newRow.push(1);
+    triangle.push(newRow);
+  }
+
+  return triangle;
 }
+
+console.log(pascalTriangle(5));
 
 module.exports = pascalTriangle;
