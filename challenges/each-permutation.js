b7ff7e344b4e6f3c1c5527318682f49a87386ee1
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-10-15 16:05:34 -0700

    initial commit

--- challenges/each-permutation.js
+++ challenges/each-permutation.js
@@ -20,10 +20,34 @@ eachPermutation([1, 2, 3], function(perm) {
 [ 3, 2, 1 ]
 */
 
-function eachPermutation(arr, callback) {
+function eachPermutation(array, callback) {
+  if (typeof callback !== 'function' || !Array.isArray(array)) return;
 
+  function findPermutations(arr) {
+    const perms = [];
+
+    // recursive base case
+    if (arr.length === 1) {
+      perms.push(arr);
+      return perms;
+    }
+
+    for (let i = 0; i < arr.length; i++) {
+      const charsLeft = arr.slice(0, i).concat(arr.slice(i + 1));
+      const innerPermutations = findPermutations(charsLeft);
+      for (let j = 0; j < innerPermutations.length; j++) {
+        const newArr = [arr[i]].concat(innerPermutations[j]);
+        // run callback if we have a correctly sized array
+        if (newArr.length === array.length) callback(newArr);
+        perms.push(newArr);
+      }
+    }
+    return perms;
+  }
+
+  findPermutations(array);
 }
 
-
+eachPermutation([1,2,3], (arr) => console.log(arr));
 
 module.exports = eachPermutation;
