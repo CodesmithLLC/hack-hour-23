60b8c9d3ba1c701940167607c9779475eb401f9d
GodofCookery <leong.bryan.c@gmail.com>
2018-11-06 16:01:17 -0800

    solved with sort method

--- challenges/highest-product.js
+++ challenges/highest-product.js
@@ -2,9 +2,13 @@
  * Given an array of integers, find the highest product you can get from three of the integers.
  */
 
-function highestProduct(array) {
+ // approach: sort the array and then take the last three elements and multiply the together.
+ // tools: sort method to get the array into lowest to highest.
+const highestProduct = (array) => {
+  let newArray = array.sort();
+  return newArray[array.length - 1] * newArray[array.length - 3];
+  }
 
-}
-
+console.log(highestProduct([1,2,3,4]));
 
 module.exports = highestProduct;
