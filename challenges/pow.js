2f6862d3ecaeb8d8dc42756e096a84b95cc01287
GodofCookery <leong.bryan.c@gmail.com>
2018-11-25 15:57:02 -0800

    recursively solved

--- challenges/pow.js
+++ challenges/pow.js
@@ -8,15 +8,33 @@
 
 // solve iteratively first.
 
+// function pow(base, power) { 
+//   let sum = base; // base = 9 in invocation. Reference for sum val at at each iteration of the for loop.
+//   //console.log(sum, "initial");
+//   for (let i = 1; i < power; i++) { 
+//     sum = base * sum;  // 81 = 9 * 9 = 81, 729 = 9 * 81. 
+//   }
+//   return sum
+// }
+
+// recursive solution.
+// set up termination and base case situations.
+// if base or power is zero then exit the function.
+// 
+// 9 * (pow(9, 3 - 1)) -> 9 * 9 = 81 
+// 9 * (pow(9, 2 - 1)) -> 9 * 81 = 729... then base case is met, recursion ends.
+
 function pow(base, power) {
-  let sum = base; // base = 9 in invocation.
-  //console.log(sum, "initial");
-  for (let i = 1; i < power; i++) { 
-    sum = base * sum;  // 81 = 9 * 9 = 81, 729 = 9 * 81. 
-  }
-  return sum
+  // termination/fail case
+  if (base < 0 || power < 0) return;
+  // base case
+  if (power === 0) return 1;
+  return base * (pow(base, power - 1));
 }
 
+console.log(pow(0, 3)); // 0
+console.log(pow(9, 0)); // 0
+console.log(pow(0, 0)); //
 console.log(pow(9, 3)); // 729 expected output;
 
 module.exports = pow;
